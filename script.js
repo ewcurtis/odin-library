b9ef0f55efaa4ba2b1d852e714d482bbfb9ab78b
Evan Curtis <ewcurtis1997@gmail.com>
2022-10-03 15:53:25 -0400

    Add card to dashboard when hitting add book button in the popup form

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const fauthor = document.querySelector("#author")
 const fpages = document.querySelector("#pages");
 const fread = document.querySelector("#read");
 const newBook = document.querySelector("#new-book");
-const cancelBook = document.querySelector("#cancel-book")
+const cancelBook = document.querySelector("#cancel-book");
+//dashboard
+const dashboard = document.querySelector(".dashboard");
 
 function Book(title, author, pages, read=false) {
     this.title = title;
@@ -23,16 +25,67 @@ function addBookToLibrary() {
     let book = new Book(ftitle.value, fauthor.value, fpages.value, fread.checked);
         myLibrary.push(book);
 
-}
-
-//Displays all books in html dashboard
-function displayLibrary() {
+        return book;
 
 }
 
 //Generates book in html dashboard
-function displayBook() {
+function displayBook(b) {
+    const bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "book-card");
+    //Delete button
+    const deleteBook = document.createElement("button");
+    deleteBook.setAttribute("id", "delete-book")
+    deleteBook.textContent = "X";
 
+    //Book Title
+    const bookTitle = document.createElement("div");
+    const titleP = document.createElement("p");
+    titleP.textContent = b.title;
+
+    bookTitle.appendChild(titleP);
+
+    //Book Author
+    const bookAuthor = document.createElement("div");
+    const authorP = document.createElement("p");
+    authorP.textContent = b.author;
+
+    bookAuthor.appendChild(authorP);
+
+    //Number of Pages
+    const bookPages = document.createElement("div");
+    const pagesH3 = document.createElement("h3");
+    pagesH3.textContent = "Pages:";
+    const pagesP = document.createElement("p");
+    pagesP.textContent = b.pages;
+
+    bookPages.appendChild(pagesH3);
+    bookPages.appendChild(pagesP);
+
+
+    //Whether the book has been read
+    const bookRead = document.createElement("div");
+    const readH3 = document.createElement("h3");
+    readH3.textContent = "Status:"
+    const readP = document.createElement("p");
+    if (b.read) {
+        readP.textContent = "Finished";
+    } else {
+        readP.textContent = "In Progress";
+    }
+
+    bookRead.appendChild(readH3);
+    bookRead.appendChild(readP);
+
+    //Append all sections to the card
+    bookCard.appendChild(deleteBook);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+
+    //Append card to dashboard
+    dashboard.appendChild(bookCard);
 }
 
 //Removes book from myLibrary and html Dashboard
@@ -49,7 +102,8 @@ bookBtn.addEventListener("click", e => {
 //Creates new book and adds it to the dashboard in html and the myLibrary array in js
 newBook.addEventListener("click", e => {
     if (ftitle.checkValidity() && fauthor.checkValidity() && fpages.checkValidity()) {
-        addBookToLibrary();
+        let book = addBookToLibrary();
+        displayBook(book);
         form.style.cssText = "display: none";
         body.removeAttribute("class", "background");
         ftitle.value = "";
